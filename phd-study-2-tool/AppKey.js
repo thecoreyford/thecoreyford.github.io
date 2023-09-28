fe0c144b85f97d6648962c2da1138f0a287e5d7c
Corey Ford <c.j.ford@qmul.ac.uk>
2023-09-28 12:42:30 +0100

    Renaming phd-study-2-tool to wAIve

--- phd-study-2-tool/AppKey.js
+++
@@ -1,5 +0,0 @@
-
-// NOTE: This file is put inside of the .gitignore file, so that when 
-// pushing and pulling to the repository they are hidden. That way 
-// no-one else can use this to mess around with your own files. 
-var access_token = 'sl.BQ_hMozRt8cSxglXvxoRwTg8p2Z3LGOyGpSTgKFPj4CPl3LhUQ0k4Ja5C2tF9ddP1oRA0vs8woaj2XhW6566NlFLeUbYo-1Sn4bMPtXMzLPVUkHgnT1-PkL5aOAS1T0WtswEJGl2zrus';
